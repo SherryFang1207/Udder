f87aa2a0076926dcbd20628f38cc96f18402276b
SherryFang1207 <zfang1207@gmail.com>
2023-11-27 16:03:39 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 房子祺 on 11/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // this is a dummy comment for testing git commit
+    }
+
+
+}
+
